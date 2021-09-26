223984830400cf33960c1606d059b8683d343f84
JBGinger <jeff.brittingham@protonmail.com>
2021-09-26 15:31:53 -0400

    Past or Present attempt 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 //Set current date at the top of the page.
 var today = moment().format('dddd, MMMM Do YYYY');
-currentDate = document.getElementById("currentDay");
-currentDate.innerHTML = today;
-window.onload = PastOrPresent();
+$("#currentDay").text(today);
 
 //Save text in each text area
 function HourClick(id) {
@@ -18,25 +16,26 @@ function FillBoxes() {
         $("#" + i).val(localStorage.getItem(lsKey))
     }
 }
-FillBoxes();
 
 //Determine whether each block is in the past, present, or future
 function PastOrPresent() {
     for (var i = 0; i < 9; i++) {
     var currentHour = moment().hour();
     var itemHour = 9 + i;
-    var currentItem = document.getElementById("#" + i);
         if (itemHour < currentHour) {
             // Show past hour formatting
-            $(currentItem).add("past");
+            $("#" + i).addClass("past");
         }
         else if (itemHour == currentHour) {
             // Show current hour formatting
-            $(currentItem).add("present");
+            $("#" + i).addClass("present");
         }
         else {
             // Show future formatting
-            $(currentItem).add("future");
+            $("#" + i).addClass("future");
         }
     }
 }
+
+PastOrPresent();
+FillBoxes();
