3c834ba3ec526d8748046a63aa4feb92e88e8993
JBGinger <jeff.brittingham@protonmail.com>
2021-09-26 15:20:53 -0400

    First attempt at past or present

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,34 +2,41 @@
 var today = moment().format('dddd, MMMM Do YYYY');
 currentDate = document.getElementById("currentDay");
 currentDate.innerHTML = today;
+window.onload = PastOrPresent();
 
 //Save text in each text area
 function HourClick(id) {
     var lsKey = "Item" + id;
-    var message = $("#Text" + id).val();
+    var message = $("#" + id).val();
     localStorage.setItem(lsKey, message);
 }
 
 //Fill text in each text area
 function FillBoxes() {
-    currentHour = moment().hour();
     for (var i = 0; i < 9; i++) {
         var lsKey = "Item" + i;
-        $("#Text" + i).val(localStorage.getItem(lsKey))
-        //Determine whether each block is in the past, present, or future
-        var itemHour = 9 + i;
-        if (itemHour < currentHour) {
-            // Show past hour formatting
-            $(item).addClass("past");
-        }
-        else if (itemHour == currentHour) {
-            // Show current hour formatting
-            $(item).addClass("present");
-        }
-        else {
-            //// Show future formatting
-            $(item).addClass("future");
-        }
+        $("#" + i).val(localStorage.getItem(lsKey))
     }
 }
 FillBoxes();
+
+//Determine whether each block is in the past, present, or future
+function PastOrPresent() {
+    for (var i = 0; i < 9; i++) {
+    var currentHour = moment().hour();
+    var itemHour = 9 + i;
+    var currentItem = document.getElementById("#" + i);
+        if (itemHour < currentHour) {
+            // Show past hour formatting
+            $(currentItem).add("past");
+        }
+        else if (itemHour == currentHour) {
+            // Show current hour formatting
+            $(currentItem).add("present");
+        }
+        else {
+            // Show future formatting
+            $(currentItem).add("future");
+        }
+    }
+}
